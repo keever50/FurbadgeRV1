597ec8fb04502d4a55137fec5a304fe96d1ecf01
Aeralius <kevinwit1999@gmail.com>
2022-07-31 13:46:31 +0200

    Add files via upload

--- python/code.py
+++ python/code.py
@@ -1,54 +1,71 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-#Importing
+# Import all board pins.
 import board
 import busio
 import digitalio
 import time
 import microcontroller
 import os
-import analogio
-
-#Badge libs/drivers
-from badge import OLED
-from badge import error
-from badge import LEDs
-
-##INIT##
-
-#Battery check
-BATV_PIN = analogio.AnalogIn(board.GP26)
-BATV = (BATV_PIN.value*6.6) / 65536
-print("Battery voltage check: BATV = {:5.2f}V".format(BATV) )
-
-if BATV >= 3:
-    print("Battery OK. Turning on OLED circuit")
-    print("[OLED]Powering on")
-    OLED.Power(True)
-    print("[OLED]Testing")
-    OLED.Test()
-    
-else:
-    print("Battery issue. Low battery voltage <3V. Stopped")
-    error.battery_low_critical()
-        
-  
+# Import the SSD1305 module.
+import adafruit_ssd1305
+# Define the Reset Pin
 
 
+#turn on 12V boost
+Booster = digitalio.DigitalInOut(board.GP24)
+Booster.direction = digitalio.Direction.OUTPUT
+Booster.value = True
 
+# Create the I2C interface.
+spi = busio.SPI(board.GP10, board.GP11)
+cs = digitalio.DigitalInOut(board.GP9)
+dc = digitalio.DigitalInOut(board.GP7)
+res = digitalio.DigitalInOut(board.GP6)
 
-#Everything ok
-print("All ok!")
-for x in range(3):
-    time.sleep(0.1)
-    LEDs.BUSY.value = True
-    LEDs.HB.value = True
-    time.sleep(0.1)
-    LEDs.BUSY.value = False
-    LEDs.HB.value = False
+# Create the SSD1305 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
 
-#hold
+display = adafruit_ssd1305.SSD1305_SPI(128, 32, spi, dc, res, cs)
+
+# Alternatively you can change the I2C address of the device with an addr parameter:
+# display = adafruit_ssd1305.SSD1305_I2C(128, 32, i2c, addr=0x31, reset=oled_reset)
+
+# Clear the display.  Always call show after changing pixels to make the display
+# update visible!
+
+def showImage(path):
+    display.fill(0)
+    file = open(path, "rb")
+    image = file.read()
+
+    b=16402
+    for Y in range(32):
+        for X in range(128):
+            b=b-4
+            pix = 255-image[b]
+            if pix > 128:
+                display.pixel(127-X, Y, pix)
+    display.show()    
+
 while True:
-    time.sleep(1)
-    LEDs.HB.value = False
-    time.sleep(1)
-    LEDs.HB.value = True    
+    dirs = os.listdir("images")
+    for file in dirs:
+        print(file)
+        showImage("images/"+file)
+        microcontroller.delay_us(3000000)
+    
+
+#for B in range(4096):
+    #display.fill_rect(B%32, round(4096/(B+1)), 1, 1, round(image[B]/255))        
+    #display.show()
+
+
+
+    #microcontroller.delay_us(1)
+    
+    #display.fill_rect(i*8,0, (i*8)+8,8, 0)
+    
+    
